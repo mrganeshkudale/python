0238648dc742f4671585d030b4fc017df07b47d5
mrganeshkudale <mrganeshkudale@gmail.com>
2020-02-17 10:33:23 +0530

    Updated index

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return 'Hello Docker World!'
+        return 'Hello Docker World Again!'
 
 api.add_resource(Greeting, '/') # Route_1
 
