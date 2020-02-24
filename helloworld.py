96ff7109110850ed9137ff9ce7608bceb89d8efe
mrganeshkudale <mrganeshkudale@gmail.com>
2020-02-24 12:30:13 +0530

    Updated index.

--- helloworld.py
+++ helloworld.py
@@ -7,15 +7,14 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-	try: 
-          host_name = socket.gethostname() 
-          host_ip = socket.gethostbyname(host_name) 
-          message = "Hostname :  "+ host_name + "IP : " + host_ip 
-	  return message 
-        except: 
-          return 'Sorry! Something is not right, let me look into it!'
+        try:
+            host_name = socket.gethostname()
+            host_ip = socket.gethostbyname(host_name)
+            message = "Hostname :  "+ host_name + "IP : " + host_ip
+            return message
+        except:
+            return 'Sorry! Something is not right, let me look into it!'
 
 api.add_resource(Greeting, '/') # Route_1
-
 if __name__ == '__main__':
-    app.run('0.0.0.0','3333')
+    app.run('0.0.0.0','8080')
