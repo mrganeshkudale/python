6421534b266778cb570962c7edbc61e88ce38b1a
mrganeshkudale <mrganeshkudale@gmail.com>
2020-06-05 17:09:21 +0530

    Added hostnames in response

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import socket
 
 app = Flask(__name__)
 api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return 'Hello Docker World Again!'
+      try: 
+        host_name = socket.gethostname() 
+        host_ip = socket.gethostbyname(host_name) 
+        print("Hostname :  ",host_name) 
+        print("IP : ",host_ip) 
+      except: 
+        print("Unable to get Hostname and IP") 
+        
+      return 'Hello Docker World Again!'
 
 api.add_resource(Greeting, '/') # Route_1
 
